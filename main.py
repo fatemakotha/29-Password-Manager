2d7b1b57e4ecbdf909e8fad3f136ca57f592007c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-25 21:16:08 +0600

    Created website_label

--- main.py
+++ main.py
@@ -15,6 +15,20 @@ logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image) #half of 200 is 100, so thats the center of the page
 canvas.pack()
 
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+
+
+
+
+
+
+#Entry
+# miles_input = Entry(width=10)
+# print(miles_input.get())
+# miles_input.grid(column=1, row=0)
+# miles_input.config(width=7)
 
 
 
