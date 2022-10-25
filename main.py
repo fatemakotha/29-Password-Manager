244bbabe76f0fa4918eccad8eed0373b0e8ee009
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-25 22:23:12 +0600

    Used focus() to bring blinking cursor into the website_entry bix

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ password_label.grid(row=3, column=0)
 #Entries
 website_entry = Entry(width=50)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus() #gets the cursor blinking in this box for user to type
 email_entry = Entry(width=50)
 email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=34)
