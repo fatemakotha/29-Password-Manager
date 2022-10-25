0fbee851676bc40123d9ef1448d52b103d49611c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-25 22:20:27 +0600

    Padding of window changed to 50

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tkinter import *
 
 window = Tk()
 window.title("My Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 
 canvas = Canvas(width=200, height=200, bg="white")
