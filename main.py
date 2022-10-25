99b2ab2f85ad77470617f456b741660b0220f7de
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-25 20:34:55 +0600

    Created a window

--- main.py
+++ main.py
@@ -3,3 +3,28 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+from tkinter import *
+
+window = Tk()
+window.title("My Password Manager")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
