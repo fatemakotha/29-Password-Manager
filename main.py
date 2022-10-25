7dbaa3c4da250feecca67eb6ee178847f6f9f3c4
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-25 20:43:02 +0600

    Added image to canvas to create background

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ window = Tk()
 window.title("My Password Manager")
 
 
-canvas = Canvas(width=200, height=200)
-
+canvas = Canvas(width=200, height=200, bg="white")
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image) #half of 200 is 100, so thats the center of the page
+canvas.pack()
 
 
 
